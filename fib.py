db981612e1838e304fd5b9f03ccefde6b30cacec
Shurikus-crypto <alex2080@yandex.ru>
2022-07-22 23:06:45 +0300

    Добавлена функция fib3 - с помощью метода мемоизации, сокращено количество вызовов.

--- fib.py
+++ fib.py
@@ -1,6 +1,9 @@
 # Вычисление чисел Фибоначчи
 
 # Бесконечная рекурсия- нет базового случая
+from typing import Dict
+
+
 def fib1(n: int) -> int:
     return fib1(n-1) + fib1(n-2)
 
@@ -13,5 +16,16 @@ def fib2(n: int) -> int:
     return fib2(n-1) + fib2(n-2)
 
 
+# Мемоизация
+# Запоминаем результаты всех вызовов
+memo: Dict[int, int] = {0: 0, 1: 1}         # два базовых случая
+
+
+def fib3(n: int) -> int:
+    if n not in memo:
+        memo[n] = fib3(n-1) + fib3(n-2)
+    return memo[n]
+
+
 if __name__ == '__main__':
-    print(fib2(10))
+    print(fib3(10))

0831e79ba5fe0628ce3e7860a8067921a7364f15
Shurikus-crypto <alex2080@yandex.ru>
2022-08-01 20:48:17 +0300

    Добавлен новый метод decompress (class CompressedGene)

--- trivial_compression.py
+++ trivial_compression.py
@@ -18,3 +18,19 @@ class CompressedGene:
                 self.bit_string |= 0b11
             else:
                 raise ValueError(f"Invalid Nucleotide: {nucleotide}")
+
+    def decompress(self) -> str:
+        gene: str = ''
+        for i in range(0, self.bit_string.bit_length()-1, 2):   # -1 чтобы исключить метку
+            bits: int = self.bit_string >> i & 0b11             # получить только 2 значимых бита
+            if bits == 0b00:
+                gene += "A"
+            elif bits == 0b01:
+                gene += "C"
+            elif bits == 0b10:
+                gene += "G"
+            elif bits == 0b11:
+                gene += "T"
+            else:
+                raise ValueError(f'Invalid bits: {bits}')
+        return gene[::-1]                                       # обращение строки

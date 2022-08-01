0cb2703ee94a7c4c5f894c90525cb0ad82b8352f
Shurikus-crypto <alex2080@yandex.ru>
2022-08-01 20:22:59 +0300

    Добавлен новый метод _compress (class CompressedGene)

--- trivial_compression.py
+++ trivial_compression.py
@@ -3,3 +3,18 @@
 class CompressedGene:
     def __init__(self, gene: str) -> None:
         self._compress(gene)
+
+    def _compress(self, gene: str) -> None:
+        self.bit_string = 1                         # начальная метка
+        for nucleotide in gene.upper():
+            self.bit_string <<= 2                   # сдвиг влево на два бита
+            if nucleotide == "A":
+                self.bit_string |= 0b00             # поменять два последних бита на 0b00
+            elif nucleotide == "C":
+                self.bit_string |= 0b01
+            elif nucleotide == "G":
+                self.bit_string |= 0b10
+            elif nucleotide == "T":
+                self.bit_string |= 0b11
+            else:
+                raise ValueError(f"Invalid Nucleotide: {nucleotide}")

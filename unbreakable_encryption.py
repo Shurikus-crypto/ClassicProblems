4524e40d0e25d31307b44b0c45a35693c56295fa
Shurikus-crypto <alex2080@yandex.ru>
2022-08-11 23:49:11 +0300

    Добавлена новая функция random_key() для генерации случайного ключа для шифровки  методом XOR.
    Signed-off-by: Shurikus-crypto <alex2080@yandex.ru>

--- unbreakable_encryption.py
+++ unbreakable_encryption.py
@@ -1,1 +1,10 @@
 # Невскрываемое шифрование с помощью оператора XOR
+from secrets import token_bytes
+
+
+def random_key(length: int) -> int:
+    """" Функция возвращает одно целое случайное число,
+         размером => length байт """
+    tb: bytes = token_bytes(length)
+    # сейчас преобразуем эту строку в битову строку
+    return int.from_bytes(tb, 'big')

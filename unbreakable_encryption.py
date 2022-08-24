75050ea36ecea2f3734e430818186d605ed4aea2
Shurikus-crypto <alex2080@yandex.ru>
2022-08-24 14:59:22 +0300

    Новая FUNCTION - encrypt, шифрует данные методом XOR

--- unbreakable_encryption.py
+++ unbreakable_encryption.py
@@ -1,10 +1,22 @@
 # Невскрываемое шифрование с помощью оператора XOR
 from secrets import token_bytes
+from typing import Tuple
 
 
 def random_key(length: int) -> int:
     """" Функция возвращает одно целое случайное число,
          размером => length байт """
     tb: bytes = token_bytes(length)
-    # сейчас преобразуем эту строку в битову строку
+    # сейчас преобразуем эту строку в целое число
     return int.from_bytes(tb, 'big')
+
+
+def encrypt(original: str) -> Tuple[int, int]:
+    original_bytes: bytes = original.encode()                   # строку в байты
+    dummy: int = random_key(len(original_bytes))
+    original_key: int = int.from_bytes(original_bytes, 'big')   # из строки байтов в целое
+    encrypted: int = original_key ^ dummy
+    return dummy, encrypted
+
+
+

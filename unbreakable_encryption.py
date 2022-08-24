ce185a3d564974c8456a0e8a33717429e72f7080
Shurikus-crypto <alex2080@yandex.ru>
2022-08-24 21:40:10 +0300

    Добавил ДОКУМЕНТАЦИЮ для функции decrypt

--- unbreakable_encryption.py
+++ unbreakable_encryption.py
@@ -32,9 +32,19 @@ def encrypt(original: str) -> Tuple[int, int]:
     return dummy, encrypted
 
 
-def decrypt(key1:int, key2: int) -> str:
+def decrypt(key1: int, key2: int) -> str:
+    """  Дешифрует строку методом XOR
+
+         :param key1: ключ шифрования
+         :type key1: int
+
+         :param key2: зашифрованная строка (порядок не имеет значения)
+         :type key2: int
+         :return: оригинальная строка
+         :rtype: str
+    """
     decrypted: int = key1 ^ key2                                # обратная операция
-    temp: bytes = decrypted.to_bytes((decrypted.bit_length()+ 7) // 8, 'big')  # из целого в байты
+    temp: bytes = decrypted.to_bytes((decrypted.bit_length() + 7) // 8, 'big')  # из целого в байты
     return temp.decode()    # из байтов в строку
 
 
